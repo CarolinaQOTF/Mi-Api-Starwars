4dd792871623e525496b6caf7cea5be763ffebc0
CarolinaQOTF <carolinacelis012@gmail.com>
2022-06-28 17:54:27 +0000

    subida de proyecto

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personajes, Planets, Vehiculos, Favoritos
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,92 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# Methods User:
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    userList = list(map(lambda obj : obj.serialize(),users))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": userList
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def show_users(user_id):
+    userId = User.query.get(user_id)
+    print(userId)
+    return jsonify(userId.serialize()), 200
+
+@app.route('/user', methods=['POST'])   
+def create_new_user():
+    body = json.loads(request.data)
+    new_user = User(nombre=body["nombre"], email=body["email"],password=body["password"])
+    db.session.add(new_user)
+    db.session.commit()
+    response_body={
+        "msg": ("usuario creado", new_user)
+    }
+    return jsonify(response_body,200)
+
+
+#Methods Personajes
+@app.route('/Personajes', methods=['GET'])
+def handle_Person():
+
+    response_body = {
+        "msg": "Hello, this is your GET /Personajes response "
+    }
+
+    return jsonify(response_body), 200
+
+#Methods Planets
+
+@app.route('/Planets', methods=['GET'])
+def handle_Planets():
+
+    response_body = {
+        "msg": "Hello, this is your GET /Planets response "
+    }
+
+    return jsonify(response_body), 200
+
+
+#Methods Vehiculos
+
+@app.route('/Vehiculos', methods=['GET'])
+def handle_Vehiculos():
+
+    response_body = {
+        "msg": "Hello, this is your GET /Vehiculos response "
+    }
+
+    return jsonify(response_body), 200
+
+
+#Methods Favoritos
+
+@app.route('/user/<int:user_id>/favoritos', methods=['GET'])
+def handle_favorites(user_id):
+    favoritos2 = Favoritos.query.filter_by(user_id = user_id)
+    listaFavoritos = list(map(lambda obj : obj.serialize(),favoritos))
+    response_body = {
+        "msg": ("Esta es la lista de favoritos:", listaFavoritos)
+    }
+    return jsonify(response_body),200
+
+@app.route('/user/<int:user_id>/favoritos/<int:favoritos_id>', methods=['DELETE'])
+def delete_favorites(user_id,favoritos_id):
+    favoritos3 = Favoritos.query.filter_by(id = favoritos_id).all()
+    print(favoritos3)
+    db.session.delete(favoritos3[0])
+    db.session.commit()
+    response_body = {
+        "msg": "Tu favorito ha sido eliminado!"
+    }
+    return jsonify(response_body),200  
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,103 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Personajes(db.Model):
+    __tablename__ = 'Personajes'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_personaje = db.Column(db.String(100), unique=True, nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    genero = db.Column(db.String(50), nullable=False)
+    color_ojos =  db.Column(db.String(50), nullable=False)
+    color_cabello = db.Column(db.String(50), nullable=False)
+    altura = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Personajes %r>' % self.nombre_personaje
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre_personaje": self.nombre_personaje,
+            "edad": self.edad,
+            "genero": self.Genero,
+            "color_ojos": self.color_ojos,
+            "color_cabello": self.color_cabello,
+            "altura": self.altura
+
+        }
+
+    
+class Planets (db.Model):
+    __tablename__ = 'Planets'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100),nullable=False)
+    clima = db.Column(db.String(50),nullable=False)
+    terreno= db.Column(db.String(250),nullable=False)
+    rotación= db.Column(db.String(250),nullable=False)
+    población= db.Column(db.String(250),nullable=False)
+    periodo_orbital= db.Column(db.String(250),nullable=False)
+    diametro= db.Column(db.String(250),nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "clima": self.climate,
+            "terreno": self.terrain,
+            "rotación": self.rotation,
+            "población": self.population,
+            "periodo_orbital": self.orbital_Period,
+            "diametro": self.diameter
+            # do not serialize the password, its a security breach
+        }
+
+class Vehiculos (db.Model):
+    __tablename__ = 'Vehiculos'
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    name_Vehicles = db.Column(db.String(50),nullable=False)
+    model = db.Column(db.String(50),nullable=False)
+
+    def __repr__(self):
+        return '<Vehiculos %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_Vehicles": self.name_Vehicles,
+            "model": self.model
+            # do not serialize the password, its a security breach
+        }
+class Favoritos (db.Model):
+    __tablename__ = 'Favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.String(250))
+    items = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Favoritos %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_user": self.id_user,
+            "items": self.items
             # do not serialize the password, its a security breach
         }
